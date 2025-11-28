0f34a6af04302c1d419a538b82ccf7489d9d55e6
vduddagi946-a11y <vduddagi946@gmail.com>
2025-11-28 10:55:27 +0530

    Delete components directory

--- components/ProductGrid.jsx
+++
@@ -1,3 +0,0 @@
-import ProductCard from './ProductCard';
-export default function ProductGrid({products}){ return (<div className="grid 
-container">{products.map(p=><ProductCard key={p.id} p={p} />)}</div>) }
