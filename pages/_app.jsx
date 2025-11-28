8c68a756e426421583176e563e36b99e7c66c366
vduddagi946-a11y <vduddagi946@gmail.com>
2025-11-28 10:56:06 +0530

    Delete pages directory

--- pages/_app.jsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import { CartProvider } from '../context/CartContext'
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <CartProvider>
-      <Component {...pageProps} />
-    </CartProvider>
-  )
-}
